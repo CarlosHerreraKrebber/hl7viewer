57d307908429762823894c9ac935d6a1c86e7229
CarlosHerreraKrebber <carlos.js.krebber@fau.de>
2025-05-27 13:32:57 +0200

    Repo for the loader and parser not working bc loader it no sting yet

--- app/src/main/java/com/example/apphl7/data/repository/HL7RepositoryImpl.kt
+++ app/src/main/java/com/example/apphl7/data/repository/HL7RepositoryImpl.kt
@@ -1,13 +1,16 @@
 package com.example.apphl7.data.repository
 
+import android.content.Context
+import com.example.apphl7.data.dataLoader.HL7FileLoader
 import com.example.apphl7.data.parser.HL7Parser
 import com.example.apphl7.domain.model.HL7Message
 import com.example.apphl7.domain.repository.HL7Repository
 
-class HL7RepositoryImpl: HL7Repository {
-
-    override fun parseHL7Content(content: String): HL7Message {
+class HL7RepositoryImpl(
+    private val loader: HL7FileLoader
+    ): HL7Repository {
+    override fun loadAndParseFromAssets(context: Context, fileName: String): HL7Message {
+        val content:String = loader.loadHL7ContentFromAssets(context, fileName)
         return HL7Parser.parse(content)
     }
-
 }
