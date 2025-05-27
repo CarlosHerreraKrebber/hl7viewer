03333d0adf472a50f8202238cf7f5fe4ec921c84
CarlosHerreraKrebber <carlos.js.krebber@fau.de>
2025-05-27 13:12:59 +0200

    create the asset to load the  samplefile in data and upload the data, return type still unit but should be string to be processabel froem the parser the context is not working properply

---
+++ app/src/main/java/com/example/apphl7/data/dataLoader/HL7FileLoader.kt
@@ -0,0 +1,10 @@
+package com.example.apphl7.data.dataLoader
+
+import android.content.Context
+
+class HL7FileLoader {
+    fun loadHL7ContentFromAssets(context: Context, fileName: String)): String {
+        val inputStream = context.assets.open(fileName)
+        return inputStream.bufferedReader().use { it.readText()}
+    }
+}
