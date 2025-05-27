84e5f8647b3a0154a785bf765047265be76a54dc
CarlosHerreraKrebber <carlos.js.krebber@fau.de>
2025-05-27 13:27:38 +0200

    Usecase, load and parse the file

--- app/src/main/java/com/example/apphl7/domain/usecase/PaseHL7FileUseCase.kt
+++ app/src/main/java/com/example/apphl7/domain/usecase/PaseHL7FileUseCase.kt
@@ -1,10 +1,11 @@
 package com.example.apphl7.domain.usecase
 
+import android.content.Context
 import com.example.apphl7.domain.model.HL7Message
 import com.example.apphl7.domain.repository.HL7Repository
 
-class ParseHL7FileUseCase(private val repository: HL7Repository) {
-    operator fun invoke(content: String): HL7Message {
-        return repository.parseHL7Content(content)
+class LoadAndParseHL7FileUseCase(private val repository: HL7Repository) {
+    operator fun invoke(context: Context, fileName: String): HL7Message {
+        return repository.loadAndParseFromAssets(context, fileName)
     }
 }
