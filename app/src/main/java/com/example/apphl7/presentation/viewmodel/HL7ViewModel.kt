ae2b919e6cf6c87fa90021ae4124b05f2b5588b5
CarlosHerreraKrebber <carlos.js.krebber@fau.de>
2025-05-27 13:31:30 +0200

    basic shoud fist launche the loader and give it the file name hold the hl7 mesage to be displade in scree

--- app/src/main/java/com/example/apphl7/presentation/viewmodel/HL7ViewModel.kt
+++ app/src/main/java/com/example/apphl7/presentation/viewmodel/HL7ViewModel.kt
@@ -1,9 +1,31 @@
 package com.example.apphl7.presentation.viewmodel
 
-import com.example.apphl7.domain.usecase.ParseHL7FileUseCase
+import android.app.Application
+import androidx.compose.runtime.*
+
+import androidx.lifecycle.AndroidViewModel
+import androidx.lifecycle.viewModelScope
+import com.example.apphl7.domain.model.HL7Message
+import com.example.apphl7.domain.usecase.LoadAndParseHL7FileUseCase
+import dagger.hilt.android.lifecycle.HiltViewModel
+import kotlinx.coroutines.launch
+import kotlinx.coroutines.Dispatchers
+import javax.inject.Inject
 
 
+@HiltViewModel
 class HL7ViewModel @Inject constructor(
-    private val parseHL7FileUseCase: ParseHL7FileUseCase
+    application: Application,
+    private val loadAndParseHL7File: LoadAndParseHL7FileUseCase
+) : AndroidViewModel(application) {
 
-)
+    var parsedMessage by mutableStateOf<HL7Message?>(null)
+        private set
+
+    init {
+        viewModelScope.launch(Dispatchers.IO) {
+            parsedMessage = loadAndParseHL7File(getApplication(), "Beispiel_HL7.hl7")
+
+        }
+    }
+}
