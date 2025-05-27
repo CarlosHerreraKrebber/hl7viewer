48e37965751b20ec5f8d11296eaf613f42e09fe1
CarlosHerreraKrebber <carlos.js.krebber@fau.de>
2025-05-27 13:34:43 +0200

    this should handle the hilt depes but some problem wiht hilt is still here

--- app/src/main/java/com/example/apphl7/di/AppModule.kt
+++ app/src/main/java/com/example/apphl7/di/AppModule.kt
@@ -1,22 +1,29 @@
 package com.example.apphl7.di
 
+import com.example.apphl7.data.dataLoader.HL7FileLoader
 import com.example.apphl7.data.repository.HL7RepositoryImpl
 import com.example.apphl7.domain.repository.HL7Repository
-import com.example.apphl7.domain.usecase.ParseHL7FileUseCase
+import com.example.apphl7.domain.usecase.LoadAndParseHL7FileUseCase
 import dagger.Module
-import dagger.InstallIn
+import dagger.Provides
+import dagger.hilt.InstallIn
+import dagger.hilt.components.SingletonComponent
+import javax.inject.SingletonallIn
 
 @Module
 @InstallIn(SingletonComponent::class)
 object AppModule {
     @Provides
     @Singleton
-    fun provideHL7Repository(): HL7Repository = HL7RepositoryImpl()
+    fun provideHL7FileLoader(): HL7FileLoader = HL7FileLoader()
 
     @Provides
     @Singleton
-    fun provideParseHL7UseCase(repo: HL7Repository): ParseHL7FileUseCase {
-        return ParseHL7FileUseCase(repo)
+    fun provideHL7Repository(loader: HL7FileLoader): HL7Repository = HL7RepositoryImpl(loader)
 
+    @Provides
+    @Singleton
+    fun provideLoadAndParseHL7FileUseCase(repo: HL7Repository): LoadAndParseHL7FileUseCase {
+        return LoadAndParseHL7FileUseCase(repo)
     }
 }
