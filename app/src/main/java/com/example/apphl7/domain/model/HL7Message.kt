c8813eee5a455fbfa11e77f1734d73f7b28e1885
CarlosHerreraKrebber <carlos.js.krebber@fau.de>
2025-05-29 14:52:51 +0200

    message and segment are now in there respectife own files for better readabilty

--- app/src/main/java/com/example/apphl7/domain/model/HL7Message.kt
+++ app/src/main/java/com/example/apphl7/domain/model/HL7Message.kt
@@ -1,10 +1,5 @@
 package com.example.apphl7.domain.model
 
-data class HL7Segment(
-    val name: String,
-    val fields: List<String>
-)
-
 data class HL7Message(
     val segments: List<HL7Segment>
 )
